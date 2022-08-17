1a8b375468dcb11166e48962168ea59e8a0d71e0
Mary Duhanina <maryduhanina@gmail.com>
2022-08-17 22:06:32 +0300

    Task5

---
+++ Task5/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine ("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine ());
+int counter = -N;//берем левый диапазон
+while (counter <=N)
+{
+    Console.WriteLine (counter + "");
+    counter++;
+}
