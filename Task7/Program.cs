0eee755142199b9947b3c5701c583f6c98861b5d
Mary Duhanina <maryduhanina@gmail.com>
2022-08-17 22:20:13 +0300

    Task7

---
+++ Task7/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine ("Введите трехзначное число: ");
+int N = Convert.ToInt32(Console.ReadLine ());
+if (N>=100&N<1000)
+{
+    Console.WriteLine (N % 10);
+}
+else
+{
+   Console.WriteLine ("Введенное число не трехзначное"); 
+}
