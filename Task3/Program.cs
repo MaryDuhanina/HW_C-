dd522407a764e03397811edbac13d874a4e81a06
Mary Duhanina <maryduhanina@gmail.com>
2022-08-17 15:43:18 +0300

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine ("Введите цифру дня недели ");
+int numberA = Convert.ToInt32(Console.ReadLine ());
+
+if (numberA >7 || numberA < 1)
+{
+    Console.WriteLine ("Нет такого дня недели");
+}
+
+if (numberA == 1)
+{
+    Console.WriteLine ("Понедельник");
+}
+if (numberA == 2)
+{
+    Console.WriteLine ("Вторник");
+}
+if (numberA == 3)
+{
+    Console.WriteLine ("Среда");
+}
+if (numberA == 4)
+{
+    Console.WriteLine ("Четверг");
+}
+if (numberA == 5)
+{
+    Console.WriteLine ("Пятница");
+}
+if (numberA == 6)
+{
+    Console.WriteLine ("Суббота");
+}
+if (numberA == 7)
+{
+    Console.WriteLine ("Воскресенье");
+}
