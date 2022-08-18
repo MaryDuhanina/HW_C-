16910b2d106d9df9f8150baf5536d55dfcc6d3d1
Mary Duhanina <maryduhanina@gmail.com>
2022-08-18 13:12:37 +0300

    Task02

---
+++ HW/Task02/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine ("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write ("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    Console.WriteLine (numberA + " max");
+    Console.WriteLine (numberB + " min");
+}
+else
+{
+  Console.WriteLine (numberA + " min");
+    Console.WriteLine (numberB + " max");  
+}
+if (numberA == numberB)
+{
+    Console.WriteLine ("Числа равны");
+}
+
