50749f9ca5d87ef73d80b016031bcd80b8fd9292
Mary Duhanina <maryduhanina@gmail.com>
2022-08-20 16:43:21 +0300

    Task04

---
+++ HW/Task04/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine ("Введите первое число ");
+int numberA = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine ("Введите второе число ");
+int numberB = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine ("Введите третье число ");
+int numberC = Convert.ToInt32(Console.ReadLine ());
+if (numberA > numberB || numberA > numberC)
+{
+    Console.WriteLine (numberA + " max");
+}
+if (numberB > numberC || numberB > numberA)
+{
+    Console.WriteLine (numberB + " max");
+}
+else
+{
+    Console.WriteLine (numberC + " max");
+}
