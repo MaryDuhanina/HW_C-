6709cbd97440b6ebafad5affafd388fa0e4227fb
Mary Duhanina <maryduhanina@gmail.com>
2022-08-20 17:06:23 +0300

    Task06

---
+++ HW/Task06/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.WriteLine ("Введите число "); 
+int numberA = Convert.ToInt32 (Console.ReadLine());
+ if (numberA % 2 == 0)
+{
+ Console.WriteLine ("Да");
+}
+ else
+{
+ Console.WriteLine ("Нет");
+}
